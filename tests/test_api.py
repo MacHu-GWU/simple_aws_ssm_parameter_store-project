f259a437c738f39e56afc90eedccbdaa5ad2917a
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-08-17 23:47:23 -0400

    Expose additional API symbols in module
    Added imports for constants, utility functions, model, and client functions to the api module to make them accessible via the api namespace. Updated tests to verify availability of these symbols.

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,6 +5,19 @@ from simple_aws_ssm_parameter_store import api
 
 def test():
     _ = api
+    _ = api.ParameterType
+    _ = api.ParameterTier
+    _ = api.ResourceType
+    _ = api.DEFAULT_KMS_KEY
+    _ = api.encode_tags
+    _ = api.decode_tags
+    _ = api.Parameter
+    _ = api.get_parameter
+    _ = api.delete_parameter
+    _ = api.get_parameter_tags
+    _ = api.remove_parameter_tags
+    _ = api.update_parameter_tags
+    _ = api.put_parameter_tags
 
 
 if __name__ == "__main__":
